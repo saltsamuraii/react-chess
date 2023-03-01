cef78a27db6e9585064bd939989cebed44a62841
Kirill_Kisel2 <kirill_kisel2@epam.com>
2023-03-01 21:02:00 +0300

    figures logic

---
+++ .history/src/models/figures/Knight_20230301201721.ts
@@ -0,0 +1,21 @@
+import { Figure, FigureNames } from './Figure';
+import { Colors } from '../Colors';
+import { Cell } from '../Cell';
+
+import blackLogo from '../../assets/black-knight.png';
+import whiteLogo from '../../assets/white-knight.png';
+
+export class Knight extends Figure {
+  constructor(color: Colors, cell: Cell) {
+    super(color, cell);
+    this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+    this.name = FigureNames.BISHOP;
+  }
+
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target)) return false;
+
+    const deltaX = Math.ab
+    return true;
+  }
+}
---
+++ .history/src/models/figures/Knight_20230301202250.ts
@@ -0,0 +1,24 @@
+import { Figure, FigureNames } from './Figure';
+import { Colors } from '../Colors';
+import { Cell } from '../Cell';
+
+import blackLogo from '../../assets/black-knight.png';
+import whiteLogo from '../../assets/white-knight.png';
+
+export class Knight extends Figure {
+  constructor(color: Colors, cell: Cell) {
+    super(color, cell);
+    this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+    this.name = FigureNames.BISHOP;
+  }
+
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target)) return false;
+
+    const deltaX = Math.abs(this.cell.x - target.x);
+    const deltaY = Math.abs(this.cell.y - target.y);
+
+
+    return (deltaX === 1 && deltaY === 2);
+  }
+}
